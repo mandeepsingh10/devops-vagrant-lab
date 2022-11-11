b79c8574b3d832c9932b1d4dff08b9000a19a4bf
mandeepsingh10 <mandeepsinghx10@gmail.com>
2022-11-11 17:41:13 +0530

    Updated config for gitlab server

--- git/provision/gitlab.sh
+++ git/provision/gitlab.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 sudo apt install ca-certificates curl openssh-server tzdata perl -y
-debconf-set-selections <<< "postfix postfix/mailname string gitlab.animals4life.local"
+debconf-set-selections <<< "postfix postfix/mailname string a4l-git.animals4life.local"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install --assume-yes postfix
 
 cd /tmp
 curl -LO https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh
 sudo bash /tmp/script.deb.sh
-sudo apt install gitlab-ce
+sudo EXTERNAL_URL="http://a4l-git.animals4life" apt-get install gitlab-ce
+sudo gitlab-ctl reconfigure
+gitlab-ctl start
+
 
 #add firewall rules
 sudo ufw allow http
